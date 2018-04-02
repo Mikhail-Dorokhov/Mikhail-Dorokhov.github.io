6e38a2bd44106424ad3e5fc806f1af7d31bae092
Mikhail Dorokhov <mikhail.dorokhov@outlook.com>
2018-04-02 01:00:28 -0400

    Foray into jQuery

---
+++ greenLive.js
@@ -0,0 +1,33 @@
+//Holds the time until the train arrives in seconds
+var pre_away = 0;
+
+//Sync with the realtime predictions every 15 seconds to maintain accuracy
+setInterval(function() {
+  $.getJSON("http://realtime.mbta.com/developer/api/v2/predictionsbystop?api_key=VFP6UU1pqE6wpD9QGpXItg&stop=70243&format=json", function(data) {
+    console.log("15 second sync")
+  })
+  .done(function(data) {
+    pre_away = data.mode[0].route[0].direction[0].trip[0].pre_away;
+  });
+},15000);
+
+//Count down seconds for prediction time.
+setInterval(function() {
+  //This if statement prevents the code from counting down into negative numbers
+  // after the T stops running
+  if (pre_away > 0) {
+    pre_away = pre_away - 1;
+    $("#green-in").text(pre_away);
+  }
+
+  //Refreshes predition time
+  if(pre_away === 0) {
+    //API call that gets a JSON object
+    $.getJSON("http://realtime.mbta.com/developer/api/v2/predictionsbystop?api_key=VFP6UU1pqE6wpD9QGpXItg&stop=70243&format=json", function(data) {
+      console.log("API request")
+    })
+    .done(function(data) {
+      //Pulls out the prediction for the nearest train
+      pre_away = data.mode[0].route[0].direction[0].trip[0].pre_away;
+    });}
+},1000);
